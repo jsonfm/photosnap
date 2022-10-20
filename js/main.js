eec568f672bc0a719fefd29d7aa87467799b06b2
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-10-20 17:12:31 -0500

    🎨 [UPDATE] header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
-})
+    navigation.classList.toggle('active');
+});
